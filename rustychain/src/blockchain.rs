acba6cefd510d4b54be6100e2eb941be31cd1ba9
gripepi <gian.ripepi96@gmail.com>
2020-05-09 13:50:52 +0100

    Created Transaction, BlockHeader, Block & BlockChain

--- rustychain/src/blockchain.rs
+++ rustychain/src/blockchain.rs
@@ -1,15 +1,39 @@
-extern crate time;
 extern crate serde;
 extern crate serde_json;
 extern crate sha2;
+extern crate time;
 
-use self::sha2::{Sha256, Digest};
 use self::serde::Serialize;
+use self::sha2::{Digest, Sha256};
 use std::fmt::Write;
 
 #[derive(Debug, Clone, Serialize)]
 struct Transaction {
     sender: String,
-    receiver: String,  
+    receiver: String,
     amount: f32,
+}
+
+#[derive(Serialize, Debug)]
+pub struct Blockheader {
+    timestamp: i64,
+    nonce: u32,
+    pre_hash: String,
+    merkle: String,
+    difficulty: u32,
+}
+
+#[derive(Serialize, Debug)]
+pub struct Block {
+    header: Blockheader,
+    count: u32,
+    transactions: Vec<Transaction>,
+}
+
+pub struct Chain {
+    chain: Vec<Block>,
+    curr_trans: Vec<Transaction>,
+    difficulty: u32,
+    miner_addr: String,
+    reward: f32,
 }
