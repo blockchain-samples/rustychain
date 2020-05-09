acfc68c1b00826306ab60ad4b6c24c8f60bc4ab8
gripepi <gian.ripepi96@gmail.com>
2020-05-09 13:39:38 +0100

    Created blockchain library && Added to main

---
+++ rustychain/src/blockchain.rs
@@ -0,0 +1,8 @@
+extern crate time;
+extern crate serde;
+extern crate serde_json;
+extern crate sha2;
+
+use self::sha2::{Sha256, Digest};
+use std::fmt::Write;
+
---
+++ rustychain/target/rls/debug/build/proc-macro2-6e4f7ab0b1b02e26/build_script_build-6e4f7ab0b1b02e26.d
@@ -0,0 +1,5 @@
+/Users/gianfrancoripepi/rustychain/rustychain/target/rls/debug/build/proc-macro2-6e4f7ab0b1b02e26/build_script_build-6e4f7ab0b1b02e26: /Users/gianfrancoripepi/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.12/build.rs
+
+/Users/gianfrancoripepi/rustychain/rustychain/target/rls/debug/build/proc-macro2-6e4f7ab0b1b02e26/build_script_build-6e4f7ab0b1b02e26.d: /Users/gianfrancoripepi/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.12/build.rs
+
+/Users/gianfrancoripepi/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.12/build.rs:
